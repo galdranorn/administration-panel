5b17cf140492e41074392208dacfafec74d555a3
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-09-11 19:25:17 +0200

    Add responsiveness to side and top menu

---
+++ js/sideMenuMobile.js
@@ -0,0 +1,9 @@
+// ---------- PAGES CHANGING ------------- 
+
+var dropdownBtn = document.getElementById('dropdown-toggle');
+var menuContent = document.getElementById('dropdown-content');
+//var postback = document.getElementById('postback');
+// functions for buttons
+dropdownBtn.addEventListener('click', function(){
+    menuContent.classList.toggle('unvisible-menu'); 
+})
